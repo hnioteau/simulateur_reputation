6f96aa1fd7bb344564f80baff131f9d5aee45719
hnioteau <niotwo@hotmail.fr>
2017-02-15 12:07:15 +0100

    squellette interface graphique

---
+++ simulateur_reputation/src/InterfaceGraphique.java
@@ -0,0 +1,22 @@
+
+
+public class InterfaceGraphique {
+	
+	public void creerCanneva(){
+		
+	}
+
+	public void afficherClient(){
+		
+	}
+	
+	public void afficherOperateur(){
+		
+	}
+	
+	public void boutonLancementSimulation(){
+		
+	}
+	
+	
+}
