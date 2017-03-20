79fea99da8430c2b4d45a49ae430c18c9142014d
hnioteau <niotwo@hotmail.fr>
2017-03-21 00:10:05 +0100

    modif interface graphique

--- simulateur_reputation/src/interfaceGraphique/InterfaceGraphique2.java
+++ simulateur_reputation/src/interfaceGraphique/InterfaceGraphique2.java
@@ -3,6 +3,7 @@ package interfaceGraphique;
 import java.awt.Color;
 import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,75 +26,111 @@ public class InterfaceGraphique2 extends JFrame {
 
 	private void build() {
 		setTitle("Simulation");
-		setSize(640, 360);
+		setSize(1000, 500);
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setContentPane(buildContentPane());
 	}
 
 	private JPanel buildContentPane() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout());
-		panel.setBackground(Color.white);
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+		mainPanel.setBackground(Color.white);
+		
+		JPanel subPanel1 = new JPanel();
+		subPanel1.setLayout(new FlowLayout());
+		subPanel1.setBackground(Color.white);
+		
+		JPanel subPanel2 = new JPanel();
+		subPanel2.setLayout(new FlowLayout());
+		subPanel2.setBackground(Color.white);
+		
+		JPanel subPanel3 = new JPanel();
+		subPanel3.setLayout(new FlowLayout());
+		subPanel3.setBackground(Color.white);
+		
+		JPanel subPanel4 = new JPanel();
+		subPanel4.setLayout(new FlowLayout());
+		subPanel4.setBackground(Color.white);
 
+		JPanel subPanel5 = new JPanel();
+		subPanel5.setLayout(new FlowLayout());
+		subPanel5.setBackground(Color.white);
+		
+		JPanel subPanel6 = new JPanel();
+		subPanel6.setLayout(new FlowLayout());
+		subPanel6.setBackground(Color.white);
+		
 		JButton btnNewButton = new JButton(new ButtonActionReset(this, "Reset simulation"));
-		panel.add(btnNewButton);
+		subPanel1.add(btnNewButton);
 
 		JButton btnAffClient = new JButton(new ButtonActionClient(this, "Mofidier client"));
-		panel.add(btnAffClient);
+		subPanel1.add(btnAffClient);
 
 		JButton btnAffOperateur = new JButton(new ButtonActionOperateur(this, "Créer operateur"));
-		panel.add(btnAffOperateur);
+		subPanel1.add(btnAffOperateur);
 
 		JButton btnChangeCas = new JButton(new ButtonActionCas(this, "Changer cas choix opérateur"));
-		panel.add(btnChangeCas);
+		subPanel1.add(btnChangeCas);
 
 		JButton btnChangeProbaEchec = new JButton(
 				new ButtonActionProbaEchec(this, "Changer méthode choix probabilité d'échec"));
-		panel.add(btnChangeProbaEchec);
+		subPanel1.add(btnChangeProbaEchec);
 
 		JButton btnSetTimer = new JButton(new ButtonSetTimer(this, "Durée de la simulation"));
-		panel.add(btnSetTimer);
+		subPanel1.add(btnSetTimer);
 		
 		JButton btnSetRho = new JButton(new ButtonSetRho(this, "Modifier rho"));
-		panel.add(btnSetRho);
+		subPanel1.add(btnSetRho);
 		
 		JButton btnSetOperator = new JButton(new ButtonSetOperator(this, "Modifier opérateur"));
-		panel.add(btnSetOperator);
+		subPanel1.add(btnSetOperator);
 
 		JButton btnStartSim = new JButton(new ButtonSimulation(this, "Lancer simulation"));
-		panel.add(btnStartSim);
+		subPanel1.add(btnStartSim);
+		
+		mainPanel.add(subPanel1);
 		
 		valRho = new JLabel();
 		valRho.setText("Valeur de rho : "+ Main.simulation.getRepFactor());
-		panel.add(valRho);
+		subPanel2.add(valRho);
+		
+		mainPanel.add(subPanel2);
 
 		timerSimulation = new JLabel();
 		timerSimulation.setText("Durée de la simulation : " + Main.simulation.getSimulationTime());
-		panel.add(timerSimulation);
+		subPanel3.add(timerSimulation);
+		
+		mainPanel.add(subPanel3);
 
 		useCase = new JLabel();
 		if (Main.simulation.getUseCase() == 0)
 			useCase.setText("Tous les opérateurs sont éligible");
 		if (Main.simulation.getUseCase() == 1)
 			useCase.setText("Seul les opérateurs qui ont assez de capacité sont éligible");
-		panel.add(useCase);
+		subPanel4.add(useCase);
+		
+		mainPanel.add(subPanel4);
 
 		echecCase = new JLabel();
 		if (Main.simulation.getProbaEchecCase() == 0)
 			echecCase.setText("Probabilité d'échec en fonction de la formule");
 		if (Main.simulation.getProbaEchecCase() == 1)
 			echecCase.setText("Probabilité d'échec fixe pour chaque opérateur");
-		panel.add(echecCase);
+		subPanel5.add(echecCase);
 
+		mainPanel.add(subPanel5);
+		
 		clientEtOperateur = new JTextArea("Client : Duree de la requete " + Main.client.getDuration()
 				+ "     Taille de la requete " + Main.client.getWeight() + "\n");
 		clientEtOperateur.setEditable(false);
 		clientEtOperateur.setOpaque(false);
-		panel.add(clientEtOperateur);
+		subPanel6.add(clientEtOperateur);
 
-		return panel;
+		mainPanel.add(subPanel6);
+		
+		return mainPanel;
 	}
 
 	static void setClientEtOperateur(String string) {
